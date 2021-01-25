c403f84288a196080073f96de991cd322a559d58
Daniil <daniil_klichkov@inbox.ru>
2021-01-25 12:29:13 +0300

    add Function Register without clearing

--- src/Register.kt
+++ src/Register.kt
@@ -1,12 +1,19 @@
 import java.io.FileWriter
 import java.io.FileReader
+import java.io.File
 import User
 
 
-fun register(user: User)
-{
-    var str = user.toString()
+fun register(user : User): String {
+    val str = user.toString()
     val fileName = "NazarLox.txt"
-    FileReader(fileName).use{}
+    val strs = File("NazarLox.txt").readLines()
+    if (str.split("~")[0] in File("NazarLox.txt").readLines()[0].split("~")){
+        FileWriter(fileName).use{it.write(File("NazarLox.txt").readLines()[0] + "~" + str.split("~")[0] + "\n")}
+        FileWriter(fileName).use{it.write(strs.joinToString(separator = "\n"))}
+        FileWriter(fileName).use{it.write(str)}
+    } else {
+        return "ERROR: this ID locked"
+    }
     FileWriter(fileName).use{it.write(str + "\n")}
 }

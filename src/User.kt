be5ca785d7d54d18da12ebcd3a04ef247b0d8d50
nazim <nazimmalyshev47@gmail.com>
2021-01-25 12:10:38 +0300

    User class v0.1

--- src/User.kt
+++ src/User.kt
@@ -1,6 +1,21 @@
-class User (var userId: Int?, var passHash: Int?, var name: String?){
+
+class User (var userId: String, var password: String, var name: String){
 
     override fun toString(): String {
-        return "$userId~$passHash~$name"
+        return "$userId~$password~$name"
+    }
+
+    fun changeName(newName: String){
+        print("Enter your password: ")
+        var passInput = readLine().toString()
+        if(passInput != password) {
+            print("Wrong password, try again.")
+        } else {
+            name = newName
+            print("Your name was successfully changed")
+        }
     }
 }
+
+
+
